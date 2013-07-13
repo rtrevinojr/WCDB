56e12b9e692b0e079bf77b7b3a41d74dc51b1e76
jameskunze <jameskunze@gmail.com>
2013-07-12 21:17:04 -0500

    Made minor changes to technical document

--- WCDB/settings.py
+++ WCDB/settings.py
@@ -17,16 +17,7 @@ DATABASES = {
         'PASSWORD': 'Ml6BaCJP8y', # Not used with sqlite3.
         'HOST': 'z', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
-        'TEST_NAME': 'cs373_zlozano',
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test_cs373_zlozano', # Or path to database file if using sqlite3.
-        'USER': 'zlozano', # Not used with sqlite3.
-        'PASSWORD': 'Ml6BaCJP8y', # Not used with sqlite3.
-        'HOST': 'z', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
-        'TEST_NAME': 'test_cs373_zlozano', # Or path to database file if using sqlite3.
+        'TEST_NAME': 'test_cs373_zlozano',
     }
 }
 
