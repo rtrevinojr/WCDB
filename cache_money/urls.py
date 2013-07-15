044258607ebcf0a8f6f0dcd6db56f6f4b0cfdfdf
jameskunze <jameskunze@gmail.com>
2013-07-15 13:40:31 -0500

    importing views in urls

--- cache_money/urls.py
+++ cache_money/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
+from wcdb import views
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
--- wcdb/views.py
+++ wcdb/views.py
@@ -2,3 +2,6 @@
 
 def export_prompt (request) :
     return render_to_response('export_prompt.html')
+
+def export_text (request) :
+    return render_to_response('export_text.html')
