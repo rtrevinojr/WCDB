aedaf316f1a4ed243ba5a36132aac6aa170e7017
Zachary Lozano <zac.lozano@gmail.com>
2013-07-15 02:19:08 -0500

    asd

--- cache_money/urls.py
+++ cache_money/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': cache_money.settings.MEDIA_ROOT}),
     url(r'^wcdb/$', TemplateView.as_view(template_name='index.html'), name="wcdb"),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
