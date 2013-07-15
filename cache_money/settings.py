6caae8dabc580180b091b52d1aeebb306e45425d
Zachary Lozano <zac.lozano@gmail.com>
2013-07-15 01:32:53 -0500

    Trying to server homepage: first attempt

--- cache_money/settings.py
+++ cache_money/settings.py
@@ -107,10 +107,13 @@ ROOT_URLCONF = 'cache_money.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'cache_money.wsgi.application'
 
+import os
+PROJ_DIR = os.path.dirname(__file__)
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJ_DIR, '../Templates'),
 )
 
 INSTALLED_APPS = (
