95615048d1a11286f938dbf925086d11f80fdbf1
jameskunze <jameskunze@gmail.com>
2013-07-15 15:13:48 -0500

    what's a close brace

--- wcdb/views.py
+++ wcdb/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render_to_response
 
 def export_prompt (request) :
-    return render_to_response('export_prompt.html', {'xml' : '<table>\n\t<nice things>\n\t\t<chocolate></chocolate>\n\t</nice things>\n</table>')
+    return render_to_response('export_prompt.html', {'xml' : '<table>\n\t<nice things>\n\t\t<chocolate></chocolate>\n\t</nice things>\n</table>'})
 
 def export_raw (request) :
-    return render_to_response('export_raw.html', {'xml' : '<table>\n\t<nice things>\n\t\t<chocolate></chocolate>\n\t</nice things>\n</table>')
+    return render_to_response('export_raw.html', {'xml' : '<table>\n\t<nice things>\n\t\t<chocolate></chocolate>\n\t</nice things>\n</table>'})
