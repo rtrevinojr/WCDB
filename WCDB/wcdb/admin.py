4273e5d9da247ce8d29874afb340758d25d994b8
tcannon91 <taylor.cannon@utexas.edu>
2013-07-15 08:54:02 -0500

    Removed old files that we dont think are needed. Taylor has a copy of files

--- WCDB/wcdb/admin.py
+++
@@ -1,7 +0,0 @@
-from wcdb.models import Crises, People, Organizations, List_Item
-from django.contrib import admin
-
-admin.site.register(Crises)
-admin.site.register(People)
-admin.site.register(Organizations)
-admin.site.register(List_Item)
--- WCDB/wcdb/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form) :
-	username = forms.CharField(max_length=100)
-	password = forms.CharField(max_length=100)
-
-class UploadFileForm(forms.Form) :
-	title = forms.CharField(max_length=50)
-	file  = forms.FileField() 
