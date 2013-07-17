6ddcbf40b0b20dfbce0dc6b23454548ef2c36c0b
tcannon91 <taylor.cannon@utexas.edu>
2013-07-17 08:47:51 -0500

    Automatically go to home

--- cache_money/urls.py
+++ cache_money/urls.py
@@ -15,6 +15,10 @@ urlpatterns = patterns('',
     #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': cache_money.settings.MEDIA_ROOT}),
     url(r'^wcdb/$', TemplateView.as_view(template_name='index.html'), name="wcdb"),
     url(r'^wcdb/org/$', TemplateView.as_view(template_name='org.html'), name="org"),
+
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+	# leave off  wcdb
+    url(r'^/$', TemplateView.as_view(template_name='index.html'), name="wcdb"),
 )
