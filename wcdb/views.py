b62f8243ad5271bad3265d3858acc86ebfc3eefc
jameskunze <jameskunze@gmail.com>
2013-07-17 14:23:01 -0500

    a

--- wcdb/views.py
+++ wcdb/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.db import DatabaseError
 from wcdb.wcdb_export import xml_mods2etree, xml_etree2xml
+from django.template.context import RequestContext
+
 
 def export_prompt (request) :
-    return render_to_response('export_prompt.html', {'xml' : get_exported_table()})
+    return render_to_response('export_prompt.html', {'xml' : get_exported_table()}, context_instance=RequestContext(request))
 
 def export_raw (request) :
     return render_to_response('export_raw.html', {'xml' : get_exported_table()})
